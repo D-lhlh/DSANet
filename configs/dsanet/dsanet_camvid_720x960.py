2851a3ba5f828e36c7443b055f811e7008d87422
D-lhlh <936103328@qq.com>
2026-04-29 10:14:51 +0800

    Update weight decay in optimizer configuration

--- configs/dsanet/dsanet_camvid_720x960.py
+++ configs/dsanet/dsanet_camvid_720x960.py
@@ -70,7 +70,7 @@ model = dict(
 )
 
 
-optimizer = dict(type='SGD', lr=0.08, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.08, momentum=0.9, weight_decay=0.0005)
 optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
 # learning policy
 iters = 80000
